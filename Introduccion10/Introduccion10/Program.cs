f6f6916601a4c21a87e96c7e6347c285d79881bf
bonepay <bonepay@gmail.com>
2023-06-01 20:53:06 +0100

    Código 01-06

---
+++ Introduccion10/Introduccion10/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                MiNota(i);
+            }
+
+            GetMiNota(2);
+
+            Console.ReadLine(); //para la consola al final
+        }
+        static public void GetMiNota(int nota)
+        {
+
+            string notaString = "";
+            switch(nota)
+            {
+
+                case 0: 
+                case 1:
+                case 2: 
+                case 3: 
+                case 4:
+                    notaString = "Suspendido";
+                    break;
+                case 5:
+                case 6: notaString = "Aprobado";
+                    break;
+                case 7: 
+                case 8: 
+                    notaString = "Notable";
+                    break;
+                case 9:
+                case 10:
+                    notaString = "Sobresaliente";
+                    break;
+                default:
+                    notaString = "Valor no esperado";
+                    break;
+            }
+        }
+
+        static public void MiNota(int nota)
+        {
+            //0 al 4; suspendido
+            //5 6: aprobado
+            //7 8: notable
+            //9 10 sobresaliente;
+            //int nota = 10;
+
+            if (nota >= 5 && nota <= 6)
+            {
+                Console.WriteLine("Aprobado");
+            }
+            if (nota >= 0 && nota < 5)
+            {
+                Console.WriteLine("Suspendido");
+            }
+            if (nota >= 7 && nota <= 8)
+            {
+                Console.WriteLine("Notable");
+            }
+            if (nota >= 9 && nota <= 10)
+            {
+                Console.WriteLine("Sobresaliente");
+            }
+        }
+    }
+
+
+}
---
+++ Introduccion12/Introduccion12/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // XXXXXXXXX
+            // X       X
+            // X       X
+            // X       X
+            // XXXXXXXXX
+           Metodo2();
+
+           Console.ReadLine();
+        }
+        static public void Metodo2()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++) 
+                {
+                    if (i == 0 || i == 9)
+                    {
+                        Console.Write("X");                       
+                    }
+                    else
+                    {
+                        if (j == 0 || j == 9)
+                        {
+                            Console.Write("X");
+                        }
+                        else 
+                        { 
+                            Console.Write(" "); 
+                        }    
+                        
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
